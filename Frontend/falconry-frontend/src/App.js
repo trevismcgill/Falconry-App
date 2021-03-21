d004ab2c34760215dcfec20396e9d1fe539e1bc3
trevismcgill <trevismcgill25@gmail.com>
2021-03-21 11:52:46 -0500

    Added packages to the frontend

--- Frontend/falconry-frontend/src/App.js
+++ Frontend/falconry-frontend/src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 
 function App() {
 	return (
